a59b9674e2f3022dbb55c2c0a56bf89460c0ad7b
Anthony Nuss <tonynuss@iastate.edu>
2021-03-08 11:49:29 -0600

    Added Username display feature. Formatted Post requests.

--- Frontend/TestGPS/app/src/main/java/com/example/testgps/utils/Const.java
+++ Frontend/TestGPS/app/src/main/java/com/example/testgps/utils/Const.java
@@ -4,8 +4,9 @@ package com.example.testgps.utils;
  * sample links for our network calls
  */
 public class Const {
-    public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
-    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+    public static final String URL_JSON_OBJECT = "http://coms-309-009.cs.iastate.edu:8080/users/1";
+   // public static final String URL_JSON_ARRAY = "http://coms-309-009.cs.iastate.edu:8080/users";
     public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
     public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 }
+//https://api.androidhive.info/volley/person_object.json
