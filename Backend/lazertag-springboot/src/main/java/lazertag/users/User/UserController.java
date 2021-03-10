365d471db45048c76af0a2d4d6ee0475eb777ef1
cgpinta <cgpinta@iastate.edu>
2021-03-09 21:31:29 -0600

    Added Delete Mapping and fixed some latitude and longitude problems

--- Backend/lazertag-springboot/src/main/java/lazertag/users/User/UserController.java
+++ Backend/lazertag-springboot/src/main/java/lazertag/users/User/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 //import lazertag.users.Team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
- @RestController
+@RestController
 public class UserController {
 
     @Autowired
@@ -67,10 +68,10 @@ public class UserController {
         return success;
     }*/
 
-    /*
+    
     @DeleteMapping(path = "/users/{id}")
-    String deleteTeam(@PathVariable int id){
+    String deleteUser(@PathVariable int id){
         userRepository.deleteById(id);
         return success;
-    }*/
+    }
 }
